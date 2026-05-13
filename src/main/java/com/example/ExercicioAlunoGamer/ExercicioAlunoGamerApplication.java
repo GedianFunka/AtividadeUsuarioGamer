07095a0ee90c420511998e8a2c04dda35fd5f574
gedianfunka <gediangabrielf@gmail.com>
2026-05-13 08:58:25 -0300

    Atividade

---
+++ src/main/java/com/example/ExercicioAlunoGamer/ExercicioAlunoGamerApplication.java
@@ -0,0 +1,14 @@
+package com.example.ExercicioAlunoGamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioAlunoGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioAlunoGamerApplication.class, args);
+	}
+
+}
+//Dedé
---
+++ src/test/java/com/example/ExercicioAlunoGamer/ExercicioAlunoGamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExercicioAlunoGamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioAlunoGamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
